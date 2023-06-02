06d35dde247fa30da3b75a402c716510f3cbaba8
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-06-02 01:21:30 -0600

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,8 @@ import { HiShoppingBag } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header className="p-[22px] flex items-start justify-between rounded-[26px] my-[18px] mx-[50px] bg-[#f5f5f5] backdrop-blur-[10px]">
+    <section  className="px-2">
+      <div className="p-4 flex items-start justify-between rounded-[26px] my-[15px] mx-auto bg-[#f5f5f5] backdrop-blur-[10px]">
       <Link to={"/"} className="font-medium font-Poppins">STOREAPP</Link>
 
       <div className="flex flex-row items-center space-x-[38px] uppercase font-Poppins font-medium">
@@ -12,7 +13,8 @@ const Header = () => {
           <HiShoppingBag />
         </Link>
       </div>
-    </header>
+    </div>
+    </section>
   );
 };
 
