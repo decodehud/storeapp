cfb580269208ad5320d632e4eecc9e05ffd1f26e
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-06-02 02:58:07 -0600

    Update root.tsx

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Inspect from "inspx";
 import Header from "../components/header";
+import Footer from "../components/footer";
 const Root = () => {
   return (
     <Inspect>
       <Header />
       <Outlet />
+      <Footer/>
     </Inspect>
   );
 };
