ef447e7be982cb8dc4bc32bdd68db1ff7041ab3b
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-06-02 01:21:41 -0600

    Update index.tsx

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,36 +1,10 @@
-import { BsArrowRightShort } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
+import Hero from "../../components/hero";
 import Card from "./card";
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const navigateToShop = () => {
-    navigate("/shop");
-  };
-
   return (
-    <div className="relative">
-      <img
-        className="absolute min-h-screen object-cover w-full -top-[180px] -z-10"
-        src="https://res.cloudinary.com/dolzrq2x7/image/upload/v1685598665/storeapp/young-person-wearing-hoodie-mockup_2_zohlk9.png"
-        alt="hero"
-      />
-
-      <div className="min-h-screen  mx-[50px]  flex flex-col justify-end items-start pb-56">
-        <p className="uppercase text-[40px] font-Poppins font-bold text-white md:text-[#151a24]">
-          Hoddie Heaven
-        </p>
-        <button
-          className="py-2 px-4 bg-[#151a24] text-white text-center rounded-xl "
-          onClick={navigateToShop}
-        >
-          <span className="flex items-center font-semibold">
-            <BsArrowRightShort />
-            <span className="ml-[10] up uppercase">shop now</span>
-          </span>
-        </button>
-      </div>
+    <div className="container mx-auto">
+      <Hero />
       <Card />
     </div>
   );
