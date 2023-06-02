63edc4a71fd2867971baa07cb3d710289033086f
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-06-02 03:36:26 -0600

    Update index.tsx

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,8 +1,10 @@
+import Hero from "../../components/hero";
 import Shop from "../shop";
 
 const Home = () => {
   return (
     <div className="container mx-auto">
+      <Hero />
       <Shop />
     </div>
   );
